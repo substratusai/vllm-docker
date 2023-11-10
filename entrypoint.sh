7df9178d31497c58b8f06724f196dced5bbb99eb
Sam Stoelinga <sammiestoel@gmail.com>
2023-11-09 22:44:20 -0600

    Quantization and extra args (#5)
    Co-authored-by: DylanAlloy <dylan@prismadic.ai>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,24 @@ if [[ -z "${MODEL}" ]]; then
     exit 1
 fi
 
+additional_args=${EXTRA_ARGS:-""}
+if [[ ! -z "${QUANTIZATION}" ]]; then
+    if [[ -z "${DTYPE}" ]]; then
+        echo "Missing required environment variable DTYPE when QUANTIZATION is set"
+        exit 1
+    else
+        additional_args="${additional_args} -q ${QUANTIZATION} --dtype ${DTYPE}"
+    fi
+fi
+
+if [[ ! -z "${GPU_MEMORY_UTILIZATION}" ]]; then
+    additional_args="${additional_args} --gpu-memory-utilization ${GPU_MEMORY_UTILIZATION}"
+fi
+
+if [[ ! -z "${MAX_MODEL_LEN}" ]]; then
+    additional_args="${additional_args} --max-model-len ${MAX_MODEL_LEN}"
+fi
+
 python3 -m vllm.entrypoints.openai.api_server \
     --tensor-parallel-size ${NUM_GPU} \
     --worker-use-ray \
@@ -17,4 +35,4 @@ python3 -m vllm.entrypoints.openai.api_server \
     --port "${PORT}" \
     --model "${MODEL}" \
     --gpu-memory-utilization "${GPU_MEMORY_UTILIZATION}" \
-    --served-model-name "${SERVED_MODEL_NAME}"
+    --served-model-name "${SERVED_MODEL_NAME}" ${additional_args}
