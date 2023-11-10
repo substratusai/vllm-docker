8ed4e0998df58e2433b84874d6ff7a94c495bdde
DylanAlloy <dylan@prismadic.ai>
2023-11-09 20:31:58 -0500

    🚧 fix(build): added quantization and subflag dtype

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,24 @@ if [[ -z "${MODEL}" ]]; then
     exit 1
 fi
 
+if ${QUANTIZATION}; then
+    if [[ -z "${DTYPE}" ]]; then
+        echo "Missing required environment variable DTYPE"
+        exit 1
+    else
+        python3 -m vllm.entrypoints.openai.api_server \
+            --tensor-parallel-size ${NUM_GPU} \
+            --worker-use-ray \
+            --host 0.0.0.0 \
+            --port "${PORT}" \
+            --model "${MODEL}" \
+            --gpu-memory-utilization "${GPU_MEMORY_UTILIZATION}" \
+            --served-model-name "${SERVED_MODEL_NAME}" \
+            --quantization "${QUANTIZATION}" \
+            --dtype "${DTYPE}"
+    fi
+fi
+
 python3 -m vllm.entrypoints.openai.api_server \
     --tensor-parallel-size ${NUM_GPU} \
     --worker-use-ray \
